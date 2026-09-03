2a933a59306963e65a3ff46f9b187247b53e4aee
umuttech <erdalozgu310@gmail.com>
2026-09-04 01:27:42 +0300

    SwankyBot Mesaj Sorunu Giderildi

--- src/commands/EĞLENCE-fakemesaj.js
+++ src/commands/EĞLENCE-fakemesaj.js
@@ -3,22 +3,23 @@ module.exports = {
     name: ['fakemesaj'], //arraya istediğiniz kadar kullanım yazabilirsiniz alieses gibi saçma bir şeyle uğraşmak yerine direk arraya ekleyebilirsiniz.
     description: "Fake mesaj gönderirsiniz.",
     kategori: "Eğlence",
-    async execute(lient, message, args) {
+    async execute(client, message, args) {
        
-  await message.delete();
+  await message.delete().catch(() => {});
   const csu = message.mentions.users.first();
   const msg = args.slice(1).join(" ");
-  if (!csu) return message.channel.send({ content: "Birisini Etiketle!"}).then(m => m.delete({ timeout: 50000 }));
-  if (!msg) return message.channel.send({ content: "Bir Mesaj Yaz!"}).then(m => m.delete({ timeout: 50000 }));
+  if (!csu) return message.channel.send({ content: "Birisini Etiketle!" }).then(m => setTimeout(() => m.delete().catch(() => {}), 5000));
+  if (!msg) return message.channel.send({ content: "Bir Mesaj Yaz!" }).then(m => setTimeout(() => m.delete().catch(() => {}), 5000));
 
-  const hook = await message.channel
-    .createWebhook(csu.username, {
-      avatar: csu.avatarURL()
-    })
-    .then(async a => {
-      await a.send({ contetnt: msg});
-      a.delete({ timeout: 100 });
+  try {
+    const hook = await message.channel.createWebhook(csu.username, {
+      avatar: csu.displayAvatarURL({ dynamic: true })
     });
+    await hook.send({ content: msg });
+    setTimeout(() => hook.delete().catch(() => {}), 1000);
+  } catch (err) {
+    console.error('[fakemesaj] Hata:', err);
+  }
       
     }
 }
